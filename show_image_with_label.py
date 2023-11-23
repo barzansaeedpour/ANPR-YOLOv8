620a477146bb19bbfba8efcba6ca88d197bb3fdc
barzansaeedpour <barzansaeedpour@gmail.com>
2023-11-23 13:25:31 +0330

    adding image visualization

--- show_image_with_label.py
+++ show_image_with_label.py
@@ -4,53 +4,39 @@ from PIL import Image
 import cv2
 
 names = {0:'License'}
-lis = open("./runs/detect/predict6/labels/frame-1-0001145.txt" , "r").readlines()
-for l in lis:
-  ind = int(l.split()[0])
-  print(ind , names[ind])
-
-li = lis[0].split()
-xc , yc , nw , nh = float(li[1]) , float(li[2]) , float(li[3]) , float(li[4])
-
-image_path = './dataset/Khanm-Rahmani/selected_mini/frame-1-0001145.jpg'
+lis = open("./runs/detect/predict6/labels/frame-1-0002175.txt" , "r").readlines()
+image_path = './dataset/Khanm-Rahmani/selected_mini/frame-1-0002175.jpg'
 img = cv2.imread(image_path)
-h , w = img.shape[0] , img.shape[1]
 
-xc *= w
-yc *= h
-nw *= w
-nh *= h
-top_left = (int(xc - nw/2) , int(yc - nh/2))
-bottom_right = (int(xc + nw/2) , int(yc + nh/2))
+for l in lis:
+    ind = int(l.split()[0])
+    print(ind , names[ind])
 
-print(top_left , bottom_right)
+    h , w = img.shape[0] , img.shape[1]
+    li = l.split()
+    xc , yc , nw , nh = float(li[1]) , float(li[2]) , float(li[3]) , float(li[4])
+    xc *= w
+    yc *= h
+    nw *= w
+    nh *= h
 
-img = cv2.rectangle(img , top_left , bottom_right , (0 , 255 , 0) , 2)
-cv2.imwrite('./temp.png',img)
-# cv2.imshow()
-# cv2.waitKey(0)
+    top_left = (int(xc - nw/2) , int(yc - nh/2))
+    bottom_right = (int(xc + nw/2) , int(yc + nh/2))
 
-# # Load the image
-# image = Image.open(image_path)
+    print(top_left , bottom_right)
 
-# # Create a figure and axis
-# fig, ax = plt.subplots(1)
+    org = (top_left[0],int(top_left[1]-(0.01*top_left[1])))
+    cv2.putText(img, text = names[ind], 
+        org = org, 
+        fontFace = cv2.FONT_HERSHEY_SIMPLEX, 
+        fontScale = 0.8,
+        color = (0 , 255 , 0),
+        thickness = 2,)
+    # img = cv2.putText(img,"Hello World!!!", top_left)
+    img = cv2.rectangle(img , top_left , bottom_right , (0 , 255 , 0) , 2)
+    
+# cv2.imwrite('./temp.png',img)
+cv2.imshow('image',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
-# # Display the image
-# ax.imshow(image)
-
-# # Define the label and bounding box coordinates (assuming label and coordinates are provided)
-# label = 'License'
-# bbox = [0.463675, 0.343213, 0.053155, 0.0241372]
-
-# # Create a rectangle patch
-# rect = patches.Rectangle((bbox[0], bbox[1]), bbox[2], bbox[3], linewidth=1, edgecolor='r', facecolor='none')
-
-# # Add the rectangle to the axis
-# ax.add_patch(rect)
-
-# # Add the label to the image
-# plt.text(bbox[0], bbox[1], label, color='r', fontsize=10, ha='left', va='top')
-
-# # Show the image with the bounding box and label
-# plt.show()
