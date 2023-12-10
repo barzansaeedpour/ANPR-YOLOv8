6bd51510c03e345156a88e3a6e2b2cdb822a844f
barzansaeedpour <barzansaeedpour@gmail.com>
2023-12-10 14:05:23 +0330

    first char detection results

--- predict.py
+++ predict.py
@@ -1,8 +1,8 @@
 from ultralytics import YOLO
 
 
-Number = 2
+Number = 15
 model = YOLO(f"./runs/detect/train{Number}/weights/best.pt")
 # model.predict(source='dataset/Khanm-Rahmani/selected_mini/', conf = 0.4, save=True, show = False, save_txt = True) 
-model.predict(source='dataset/iraninan/car/', conf = 0.10, save=True, show = False, save_txt = True) 
+model.predict(source='dataset/char3/test/images', conf = 0.10, save=True, show = False, save_txt = True) 
   
