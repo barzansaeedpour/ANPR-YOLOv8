b5aa7136244ccbdd0c9a970e8d9d8bff6af5243f
barzansaeedpour <barzansaeedpour@gmail.com>
2023-12-11 14:42:28 +0330

    adding new videos

--- videos-to-frames.py
+++ videos-to-frames.py
@@ -1,11 +1,12 @@
 import cv2
 import os
 
-for N in [10]:
-    vidcap = cv2.VideoCapture(f'./dataset/Khanm-Rahmani/{N}.mp4')
+for N in [0,1,2,3]:
+    video_path= f'./dataset/videos/sanandaj0{N}.mp4'
+    vidcap = cv2.VideoCapture(video_path)
     success,image = vidcap.read()
     count = 0
-    target_path = f"./dataset/Khanm-Rahmani/frames/"
+    target_path = f"./dataset/sanandaj/"
     os.makedirs(target_path, exist_ok=True)
 
     # fps = 30
