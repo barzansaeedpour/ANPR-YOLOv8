52d245440b19c74d3f57c22fd8ef6f5915bf0bed
barzansaeedpour <barzansaeedpour@gmail.com>
2023-12-14 16:19:53 +0330

    fixing some bugs

--- detect_on_webcam.py
+++ detect_on_webcam.py
@@ -13,15 +13,18 @@ cap.set(3, 640)
 cap.set(4, 480)
 
 # model
-model = YOLO("./runs/detect/train2/weights/best.pt")
+model = YOLO("./runs/detect/train19/weights/best.pt")
 
-classNames = [
-              'پلاک',
-              ]
+# classNames = [
+#               'پلاک',
+#               ]
+classNames = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'be', 'dal', 'gaf', 'ghaf', 'h', 'he', 'jim', 'lam', 'mim', 'noon', 'sad', 'sin', 'ta', 'te', 'waw', 'ye']
+
 
 
 def persian(text):
-    return get_display(arabic_reshaper.reshape(text))
+    # return get_display(arabic_reshaper.reshape(text))
+    return text
 
 
 while True:
@@ -52,11 +55,13 @@ while True:
             draw = ImageDraw.Draw(img)
             draw.rectangle([(x1, y1), (x2, y2)], outline ='red',)
 
-            font = ImageFont.truetype("./fonts/BZar.ttf", size=23)
+            # font = ImageFont.truetype("./fonts/BZar.ttf", size=23)
+            # font = ImageFont.truetype("./fonts/BZar.ttf", size=23)
             text = classNames[cls]
             color = (0, 0, 255)  # Red color
             draw.rectangle([(org[0], org[1]), (org[0]+(len(text)*15), org[1]+25)], fill =(255,100,100))
-            draw.text(org, persian(text), fill=(255,255,255), font=font)
+            # draw.text(org, persian(text), fill=(255,255,255), font=font)
+            draw.text(org, persian(text), fill=(255,255,255))
             img = np.array(img)
 
 
