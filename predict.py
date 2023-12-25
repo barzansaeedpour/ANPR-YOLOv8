6cba39576087c80c4ea4dfbaa146689d67ddd0c3
barzansaeedpour <barzansaeedpour@gmail.com>
2023-12-25 13:44:17 +0330

    working on new data

--- predict.py
+++ predict.py
@@ -1,8 +1,9 @@
 from ultralytics import YOLO
 
 
-Number = 15
-model = YOLO(f"./runs/detect/train{Number}/weights/best.pt")
+# Number = 15
+# model = YOLO(f"./runs/detect/train{Number}/weights/best.pt")
+model = YOLO(f"./models/plate-detector.pt")
 # model.predict(source='dataset/Khanm-Rahmani/selected_mini/', conf = 0.4, save=True, show = False, save_txt = True) 
-model.predict(source='dataset/char3/test/images', conf = 0.10, save=True, show = False, save_txt = True) 
+model.predict(source='dataset/tehran_selected/', conf = 0.10, save=False, show = False, save_txt = True) 
   
