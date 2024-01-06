a848588c2f845cd916a50ca17890fab129644947
barzansaeedpour <barzansaeedpour@gmail.com>
2024-01-06 11:59:44 +0330

    working on the new dataset

--- train.py
+++ train.py
@@ -6,3 +6,4 @@
 # results = model.train(data='dataset-all-yolo2/data_custom.yaml', epochs=2, imgsz=300, batch=2)
 
 # yolo task=detect mode=train epochs=300 data='./dataset/char-all2/data.yaml' model=yolov8m.pt imgsz=300 batch=2
+# yolo task=detect mode=train epochs=300 data='./dataset/char-all/data.yaml' model=yolov8m.pt imgsz=300 batch=2
